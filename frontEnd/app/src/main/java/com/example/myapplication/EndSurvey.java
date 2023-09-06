c1a9c3eb10526e3a7af5c6a7c2de4e096498c744
jiajuny2 <825897206@qq.com>
2023-09-06 16:24:35 +1000

    update text file in String

--- frontEnd/app/src/main/java/com/example/myapplication/EndSurvey.java
+++ frontEnd/app/src/main/java/com/example/myapplication/EndSurvey.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.bumptech.glide.Glide;
 
 public class EndSurvey extends AppCompatActivity {
