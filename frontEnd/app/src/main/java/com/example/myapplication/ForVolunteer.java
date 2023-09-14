409bfda47c86d80d6301d0e034040418f312b9ae
zixunq <zixunq@student.unimelb.edu.au>
2023-09-14 19:43:22 +1000

    add for leader page

---
+++ frontEnd/app/src/main/java/com/example/myapplication/ForVolunteer.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ForVolunteer extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.for_volunteer);
+    }
+
+    // tool bar
+}
