04e9de934a2f3a6ea915c9400f9fc75ad0bee660
zixunq <zixunq@student.unimelb.edu.au>
2023-10-11 20:59:31 +1100

    add learn more to activity and transition to brochure

--- frontEnd/app/src/main/java/com/example/myapplication/LearnMore.java
+++ frontEnd/app/src/main/java/com/example/myapplication/LearnMore.java
@@ -1,7 +1,11 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.content.Intent;
+import android.net.Uri;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +18,18 @@ public class LearnMore extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.learn_more);
 
-
+        Button brochure = findViewById(R.id.brochure);
+        brochure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openBrochurePage();
+            }
+        });
+    }
+    private void openBrochurePage(){
+        String pdfUrl = "https://volpoll.org.au/wp-content/uploads/2020/05/VolPoll_WEB.pdf";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(pdfUrl));
+        startActivity(intent);
     }
 }
